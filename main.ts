602d9d908f39fc0360e30fe1fb0a5e5d5df130c8
GentlemanJimMaguire <124887271+GentlemanJimMaguire@users.noreply.github.com>
2023-02-23 11:06:12 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let Comp = 0
+let MyGo = 0
+input.onGesture(Gesture.Shake, function () {
+    Comp = randint(1, 3)
+    if (Comp == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else if (Comp == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+    MyGo = 0
+})
